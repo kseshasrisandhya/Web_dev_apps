d1fb89ae0af57f17e01516ff72799d4620e41836
SriSandhya <ksrisandhya.9@msitprogram.net>
2020-04-16 23:55:10 +0530

    PROJECT_1 UPDATE

--- Assignment_web/project1/application.py
+++ Assignment_web/project1/application.py
@@ -5,21 +5,38 @@ from flask import Flask, session,render_template,Response,request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from regis import *
+from datetime import datetime
+
+# 
+
+
+   
 
 app = Flask(__name__)
+# db=SQLAlchemy(app)
+
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
 
 # Configure session to use filesystem
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+# app.config["SESSION_PERMANENT"] = False
+# app.config["SESSION_TYPE"] = "filesystem"
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# Session(app)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
-db = scoped_session(sessionmaker(bind=engine))
+
+# engine = create_engine(os.getenv("DATABASE_URL"))
+# db = scoped_session(sessionmaker(bind=engine))
+db.init_app(app)
+
+
 
 
 @app.route("/")
@@ -28,8 +45,21 @@ def index():
 
 @app.route("/print",methods=["POST","GET"])
 def print1():
-    first=request.form.get("Fname")
-    last=request.form.get("Lname")
-    print(first,last)
-    return render_template("print.html",first=first,last=last)
+    Registrations.query.all()
+    first = request.form.get("Fname")
+    last = request.form.get("Lname")
+    email = request.form.get("Email")
+    regist = Registrations(FIRSTNAME=first, LASTNAME=last,EMAIL=email,datetime=datetime.now())
+    try:
+        db.session.add(regist)
+        db.session.commit()
+        print(first+" "+email)
+        return render_template("print.html",first=first,EMAIL = email)
+    except Exception :
+	    return render_template("error.html", errors = "Details are already given")
+
+
+    
+        
+ 
     
---
+++ Assignment_web/project1/regis.py
@@ -0,0 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Registrations(db.Model):
+    __tablename__ = "Registration"
+    FIRSTNAME = db.Column(db.String, nullable=False)
+    LASTNAME = db.Column(db.String, nullable=False)
+    EMAIL = db.Column(db.String, primary_key=True)
+    datetime =db.Column(db.String,nullable=False)
+      
