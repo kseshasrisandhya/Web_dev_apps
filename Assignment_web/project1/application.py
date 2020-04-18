28d4eb71be380f45e4a3c1c84cb69545817738aa
SriSandhya <ksrisandhya.9@msitprogram.net>
2020-04-19 01:05:54 +0530

    project_1 updates

--- Assignment_web/project1/application.py
+++ Assignment_web/project1/application.py
@@ -1,7 +1,7 @@
 import os
 import logging
 import sys
-from flask import Flask, session,render_template,Response,request
+from flask import Flask, session,render_template,Response,request,redirect,url_for
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -36,12 +36,13 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 # db = scoped_session(sessionmaker(bind=engine))
 db.init_app(app)
 
+app.secret_key = "any random string"
 
 
-
-@app.route("/")
-def index():
+@app.route("/register")
+def register():
     return render_template("Registration.html")
+
 
 @app.route("/print",methods=["POST","GET"])
 def print1():
@@ -57,6 +58,60 @@ def print1():
         return render_template("print.html",first=first,EMAIL = email)
     except Exception :
 	    return render_template("error.html", errors = "Details are already given")
+
+@app.route("/admin")
+def admin():
+    Register=Registrations.query.all()
+    return render_template("admin.html",regist=Register)
+
+@app.route('/')
+def index():
+   if 'user' in session:
+        user = session['user']
+        return 'Logged in as ' + user + '<br>' + \
+        "<b><a href = '/logout'>click here to log out</a></b>"
+   return "You are not logged in <br><a href = '/login'></b>" + \
+    "click here to log in</b></a>"
+
+
+@app.route("/auth",methods = ["GET","POST"])
+def authenticate():
+    Registrations.query.all()
+    name = request.form.get("Fname")
+    email = request.form.get("Email")
+    log = Registrations(FIRSTNAME = name  ,EMAIL = email)
+    try:
+        Member = db.session.query(Registrations).filter(Registrations.EMAIL == email).all()
+        print(Member[0].FIRSTNAME)
+        session['user'] = request.form.get("Email")
+        # return render_template("print.html",first=name,EMAIL = email)
+        return redirect(url_for('index'))
+    except Exception :
+	    return render_template("error.html", errors = "Details are already given")   
+
+
+      
+
+# @app.route('/login', methods = ['GET', 'POST'])
+# def login():
+#    if request.method == 'POST':
+      
+#       return redirect(url_for('index1'))
+#    return '''
+	
+#    <form action = "" method = "post">
+#       <p><input type = text name = username/></p>
+#       <p<<input type = submit value = Login/></p>
+#    </form>
+	
+#    '''
+
+@app.route('/logout')
+def logout():
+   # remove the username from the session if it is there
+   session.pop('user', None)
+   return redirect(url_for('register'))
+
 
 
     
